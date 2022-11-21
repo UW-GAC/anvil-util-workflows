154394b9a2affb4f25a02730e1226d0f70a0547e
Stephanie M. Gogarten <sdmorris@uw.edu>
2022-11-21 10:31:58 -0800

    name of DD table not required to be 'data'; take from JSON

--- data_dictionary_report.R
+++ data_dictionary_report.R
@@ -5,18 +5,18 @@ argp <- arg_parser("report")
 argp <- add_argument(argp, "--data_file", help="tsv file with data")
 argp <- add_argument(argp, "--dd_file", help="json file with data dictionary for GSR files")
 argp <- add_argument(argp, "--out_prefix", help="output prefix")
-argv <- parse_args(argp)
 
 # argv <- list(data_file="testdata/gsr_chr1.tsv",
 #              dd_file="testdata/gsr_dd.json",
 #              out_prefix="test")
 
-# read 1000 rows for checking data against expected type
-dat <- readr::read_tsv(argv$data_file, n_max=1000)
-dat <- list(data=dat)
-
 # read data model
 dd <- json_to_dm(argv$dd_file)
+
+# read 1000 rows for checking data against expected type
+dat <- readr::read_tsv(argv$data_file, n_max=1000)
+dat <- list(dat)
+names(dat) <- names(dd)
 
 outfile <- paste0(argv$out_prefix, ".txt")
 con <- file(outfile, "w")
@@ -25,7 +25,7 @@ pass <- TRUE
 chk <- check_column_names(dat, dd)
 res <- parse_column_name_check(chk)
 if (nrow(res) > 0) {
-    if (length(chk$data$missing_required_columns) > 0) pass <- FALSE
+    if (length(chk[[1]]$missing_required_columns) > 0) pass <- FALSE
     writeLines(knitr::kable(res[,-1]), con)
     writeLines("\n", con)
 }
